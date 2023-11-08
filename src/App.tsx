73de8e533040072e0465f1551837c24f4697b2fe
Sudarshan S <senthilsudarshan@gmail.com>
2023-11-08 21:31:38 +0530

    design complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import './App.css';
+import "./App.css";
+import CodeDisplay from "./Components/CodeDisplay";
+import MessagesDisplay from "./Components/MessagesDisplay";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+	return (
+		<div className="App">
+			<MessagesDisplay />
+
+			<input />
+			<CodeDisplay />
+			<div className="button-container">
+				<button id="get-query">Get Query!</button>
+				<button id="clear-chat">Clear Chat</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Components/MessagesDisplay.tsx
@@ -0,0 +1,12 @@
+import MessageDisplay from "./MessageDisplay";
+
+function MessagesDisplay() {
+	return (
+		<div className="messages-display">
+			<MessageDisplay />
+			<MessageDisplay />
+		</div>
+	);
+}
+
+export default MessagesDisplay;
